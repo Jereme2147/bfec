498aabcc3bbe6c4d81a63fef810734fc92149d31
Jereme <jereme.daniels@gmail.com>
2019-05-23 15:43:18 -0400

    mobile and desktop landing started

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 //hamburger function
+// opens the mobile menu
 const icon = document.getElementsByClassName('icon');
+const menuOpen = document.getElementById('mobile-menu-open');
 for (let i = 0; i < icon.length; i++){
     icon[i].addEventListener('click', function () {
         icon[i].classList.toggle("active");
+        if (menuOpen.style.opacity == "1") {
+            menuOpen.style.opacity = '0';
+            menuOpen.style.pointerEvents = 'none';
+        } else {
+            menuOpen.style.opacity = "1";
+            menuOpen.style.pointerEvents = "auto";
+        }
     });
 };
  
+
