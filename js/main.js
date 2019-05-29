d0963a534fc04f1e3c9682c8d4edc3277868a517
Jereme <jereme.daniels@gmail.com>
2019-05-29 14:15:44 -0400

    starting php converstion

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 //hamburger function
 // opens the mobile menu
+let tablet = window.matchMedia("(min-width: 600px)");
+let desktop = window.matchMedia("(min-width: 950px)");
+
 const icon = document.getElementsByClassName('icon');
 const menuOpen = document.getElementById('mobile-menu-open');
 for (let i = 0; i < icon.length; i++){
@@ -14,5 +17,38 @@ for (let i = 0; i < icon.length; i++){
         }
     });
 };
- 
 
+//functions for landing page image loading.
+//function to determine if an image is within the viewport. 
+// not called until scroll
+var isInViewport = function (elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+const imgtest = document.getElementById('imgtest');
+//on scroll, calls the isInViewport function. If image is within the viewport
+//it expands the picture.
+window.addEventListener('scroll', function (event) {
+    const sideLoad = document.getElementsByClassName('side-load');
+    for (let i = 0; i < sideLoad.length; i++) {
+        //checks to see if all images are within viewport. 
+        if (isInViewport(sideLoad[i])) {
+            if(sideLoad[i].style.visibility != 'none'){
+                if(isInViewport(sideLoad[i])){
+                    if(desktop.matches){
+                        //calls only on desktop version
+                        sideLoad[i].style.width = '75%';
+                    } else {
+                        //calls on anything < 600px
+                        sideLoad[i].style.width = '75%';
+                    }
+                }
+            }
+        };
+    }
+});
