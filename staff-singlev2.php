43a79dc34bf8784ebb81dcbe60ecb3ef456303c1
Jereme <jereme.daniels@gmail.com>
2019-06-11 19:32:21 -0400

    almost ready to start wordpress

---
+++ staff-singlev2.php
@@ -0,0 +1,55 @@
+<?php
+    include './header.php';
+?>
+<div class="no-banner-spacer">
+
+</div>
+<div class="staff-single-container">
+    <div class="staff-single-title-image-bio">
+        <div class="staff-single-name-title">
+            <h3>Josselyn Lucas</h3>
+            <h4>-Project Manager</h4>
+        </div>
+        <div class="staff-single-image">
+            <img src="./staff/joseelyn-single-square.jpg" alt="">
+        </div>
+    
+        <div class="staff-single-bio">
+            <p>“I’ve always had a passion for the outdoors. As a kid, I spent my summers flipping rocks in the creek next to my childhood home looking for salamanders and crawdads. I consider myself lucky to work in a field that allows me to realize my passion for nature through the enhancement and improvement of the sensitive habi‐ tats of the Appalachians. When I’m not working I’m playing with my dog, hiking, kayaking or just enjoying my downtime.”</p>
+        </div>
+    </div>
+    <div class="staff-single-qual-expert">
+        <div class="staff-single-qualifications">
+            <h3>Qualifications and Experience</h3>
+            <ul>
+                <li>B.S. ‐ Evolution, Ecology, and Environmental Biology Appalachian State University</li>
+                <li>River Morphology and Applications Wildland Hydrology, 2019</li>
+                <li>Tennessee Stream Quantification Tool Workshop (TN SQT) Will Harmon/TDEC, 2018</li>
+                <li>Applied Fluvial Geomorphology Wildland Hydrology, 2018</li>
+                <li>USACE Wetland Delineation Certification Richard Chinn Environmental Training, 2018</li>
+                <li>WPIT Certification
+                Society of Wetland Scientists, 2017</li>
+                <li>Project WET (Water Education for Teachers) certification NCDNCR, 2014</li>
+                <li>Certification in Geographical Information Science (GIS) Appalachian State, 2014</li>
+            </ul>
+        </div>
+            <div class="staff-single-expertise">
+                <h3>Areas of Expertise:</h3>
+                <ul>
+                    <li>Project Management (4+ Years Experience)</li>
+                    <li>Restoration Ecology (Riverine/Wetland Habitat)</li>
+                    <li>Wetland and Stream Assessment and Delineations</li>
+                    <li>Natural Channel Stream Design</li>
+                    <li>401/404 Permitting</li>
+                    <li>nnovative Stormwater Solutions</li>
+                    <li>On‐Site Consultations</li>
+                    <li>Mitigation Banking Site Selection and Implementation</li>
+                    <li>Environmental Site Assessments</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+    include './footer.php';
+?>
